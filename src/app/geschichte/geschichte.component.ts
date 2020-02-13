310a94a1aba478937197b6fdc8bc1b9469d6d911
GTDominic <seitheldominic@gmail.com>
2020-02-13 08:00:55 +0100

    added geschichte tabl (data still has to be added)

--- src/app/geschichte/geschichte.component.ts
+++ src/app/geschichte/geschichte.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface GeschichteTabelle {
+  year: string;
+  month: string;
+  event: string;
+}
+
+// {year:'' , month: '', event: ''},
+const GESCHICHTE_DATA: GeschichteTabelle[] = [
+  {year: '1975', month: 'März', event: `Von Ratsmitgliedern wird anlässlich einer Landtagswahl
+     die Idee geboren, aus Mitteln des Waldverkaufs Instrumente für eine zu gründende
+     Blaskapelle anzuschaffen. Diese Anregung entstand beim Betrachten von Bildern,
+     auf denen schon vor dem Krieg Neuleininger Musiker den Stabaus begleiteten. Der
+     damalige Bürgermeister Helmut Rüttger und die Ratsmitglieder Edgar Krämer und
+     Herbert Kröner versprachen Hilfe oder aktive Unterstützung.`},
+];
+
 @Component({
   selector: 'app-geschichte',
   templateUrl: './geschichte.component.html',
   styleUrls: ['./geschichte.component.css']
 })
 export class GeschichteComponent implements OnInit {
+
+  displayedGColumns: string[] = ['year', 'month', 'event'];
+  geschichteSource = GESCHICHTE_DATA;
 
   constructor() { }
 
