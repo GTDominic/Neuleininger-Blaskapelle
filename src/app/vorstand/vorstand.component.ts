4c64cee1d299633a0d113b98504ebf7247d11fca
GTDominic <seitheldominic@gmail.com>
2020-05-12 11:15:09 +0200

    Vorstand added from database

--- src/app/vorstand/vorstand.component.ts
+++ src/app/vorstand/vorstand.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MusikerService } from '../services/musiker.service';
 
 @Component({
   selector: 'app-vorstand',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VorstandComponent implements OnInit {
 
-  constructor() { }
+  musikers: any;
+
+  constructor(private musikerService: MusikerService) { }
 
   ngOnInit() {
+    this.getMusiker();
+  }
+
+  getMusiker() {
+    this.musikerService.getMusiker()
+      .subscribe(
+        data => {
+          this.musikers = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
