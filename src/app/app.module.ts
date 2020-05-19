087a75dd9742af36f0929abcbf66a87f0f06c0a9
GTDominic <seitheldominic@gmail.com>
2020-05-19 14:38:04 +0200

    Added amazing time picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './MaterialModule';
 import { HttpClientModule } from '@angular/common/http';
+import { AmazingTimePickerModule } from 'amazing-time-picker';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -30,6 +31,7 @@ import { VorstandComponent } from './vorstand/vorstand.component';
     MaterialModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    AmazingTimePickerModule,
     HttpClientModule
   ],
   providers: [],
