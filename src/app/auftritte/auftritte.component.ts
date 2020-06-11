343251bfeb742776d0c368d679f1cc3a822da0f3
GTDominic <seitheldominic@gmail.com>
2020-06-11 08:08:54 +0200

    Presave (buggy)

--- src/app/auftritte/auftritte.component.ts
+++ src/app/auftritte/auftritte.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { AuftritteService } from '../services/auftritte.service';
 
+export interface AuftritteInterface {
+  ort?: String;
+  event?: String;
+  date?: String;
+  start?: String;
+  end?: String;
+}
+
 @Component({
   selector: 'app-auftritte',
   templateUrl: './auftritte.component.html',
   styleUrls: ['./auftritte.component.css']
 })
+
 export class AuftritteComponent implements OnInit {
 
-  actauftritte: any;
-  displayedColumns = ['location', 'event'];
+  dbauftritte: any;
+  actauftritte = {} as Array<AuftritteInterface>;
+  
+  displayedColumns = ['location', 'event','start'];
 
   constructor(private auftritteService: AuftritteService) { }
 
@@ -21,11 +32,17 @@ export class AuftritteComponent implements OnInit {
     this.auftritteService.getAuftritte()
       .subscribe(
         data => {
-          this.actauftritte = data;
+          this.dbauftritte = data;
           console.log(data);
+          this.actauftritte.length = this.dbauftritte.length;
+          
+          // for(let i = 0; i < this.dbauftritte.length; i++){
+          //   this.actauftritte[i].ort = this.dbauftritte[i].location;
+          //   console.log(this.actauftritte);
+          // }
         },
         error => {
           console.log(error);
-        });
+        });    
   }
 }
