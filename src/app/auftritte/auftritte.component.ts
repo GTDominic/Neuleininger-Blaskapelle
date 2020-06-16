eb6dd0d7aaa5f3c06823f709205045026cfd2fb0
GTDominic <seitheldominic@gmail.com>
2020-06-16 10:32:15 +0200

    finished date formatting

--- src/app/auftritte/auftritte.component.ts
+++ src/app/auftritte/auftritte.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
 import { AuftritteService } from '../services/auftritte.service';
 
 export interface AuftritteInterface {
@@ -19,8 +20,9 @@ export class AuftritteComponent implements OnInit {
 
   dbauftritte: any;
   actauftritte: AuftritteInterface[] = [];
+  showtable: boolean = false;
 
-  displayedColumns = ['location', 'event', 'start'];
+  displayedColumns = ['ort', 'event','date', 'start', 'end'];
 
   constructor(private auftritteService: AuftritteService) { }
 
@@ -36,14 +38,25 @@ export class AuftritteComponent implements OnInit {
           console.log(this.dbauftritte);
           // tslint:disable-next-line:prefer-for-of
           for (let i = 0; i < this.dbauftritte.length; i++) {
+            // convert utc time to unix
+            let tempstartdb = Date.parse(this.dbauftritte[i].start);
+            let tempenddb = Date.parse(this.dbauftritte[i].end);
+
+            // convert unix dates into user dates
+            let tempdate = formatDate(tempstartdb, 'd.M.y', 'en-US');
+            let tempstart = formatDate(tempstartdb, 'HH:mm', 'en-US');
+            let tempend = formatDate(tempenddb, 'HH:mm', 'en-US');
+
+            // put dates into table array
             this.actauftritte.push({
               ort: this.dbauftritte[i].location,
               event: this.dbauftritte[i].name,
-              date: '3',
-              start: '4',
-              end: '5'
+              date: tempdate,
+              start: tempstart,
+              end: tempend
             });
           }
+          this.showtable = true;
         },
         error => {
           console.log(error);
